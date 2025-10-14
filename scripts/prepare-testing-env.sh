9060fbc6e3a312662816cc12c51029c313df77c9
Michael Werner <michael@werner.io>
2025-10-14 11:14:42 -0700

    fix: bump kubernetes version (previous one unsupported)

--- scripts/prepare-testing-env.sh
+++ scripts/prepare-testing-env.sh
@@ -2,5 +2,5 @@
 
 set -eo pipefail
 
-minikube start --driver=docker --kubernetes-version=v1.24.10 -p kibernate-test
+minikube start --driver=docker --kubernetes-version=v1.33.3 -p kibernate-test
 minikube profile kibernate-test
